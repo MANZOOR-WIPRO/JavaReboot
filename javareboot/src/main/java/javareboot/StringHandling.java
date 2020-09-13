d561740ce5f29ee2fc4cb39078f761deb381e341
MANZOOR_WIPRO <manzurulqayyum@gmail.com>
2020-09-13 16:57:42 +0530

    initial

--- javareboot/src/main/java/javareboot/StringHandling.java
+++ javareboot/src/main/java/javareboot/StringHandling.java
@@ -1,10 +1,10 @@
-package com.bacics.javareboot;
+package javareboot;
 
-public class TestClass {
+public class StringHandling {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello");
+System.out.println("Hi");
 	}
 
 }
